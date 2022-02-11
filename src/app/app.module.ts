b2cdb3dd0f0da81f9e2f6a354c36090c0250999c
brcaipr <braiantorres228@gmail.com>
2022-02-11 15:44:05 -0300

    Primer Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { FotoperfilComponent } from './components/fotoperfil/fotoperfil.component';
+import { LogoApComponent } from './components/logo-ap/logo-ap.component';
+import { RedesComponent } from './components/redes/redes.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    FotoperfilComponent,
+    LogoApComponent,
+    RedesComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
